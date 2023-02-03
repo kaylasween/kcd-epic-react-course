e6392b7371fd35bd3f1615c69ca850884d26f761
Kayla Sween <kayla@Kaylas-Flightcontrol-Macbook-Pro-2.local>
2023-02-03 14:50:17 -0600

    finishing extra credit 1, fixing bug from exercise.

--- react-hooks/src/exercise/06.js
+++ react-hooks/src/exercise/06.js
@@ -2,29 +2,32 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {fetchPokemon, PokemonInfoFallback, PokemonDataView, PokemonForm} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
 
   React.useEffect(() => {
     if(!pokemonName) {
       return
     }
     setPokemon(null)
-
+    setError(null)
     fetchPokemon(pokemonName).then(
       pokemon => {
-        setPokemon(pokemonName)
+        setPokemon(pokemon)
       }
-    )
+    ).catch(err => setError(err))
   }, [pokemonName])
 
-  if(!pokemonName) {
+  if(error) {
+    return (
+      <div role="alert">
+        There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  } else if (!pokemonName) {
     return 'Submit a Pokemon!'
   } else if (!pokemon) {
     return <PokemonInfoFallback name={pokemonName} />
