b2f30f237b7a3c3b752dc2b7437fdbd5c5c4e98d
Kayla Sween <kayla@Kaylas-Flightcontrol-Macbook-Pro.local>
2023-02-07 12:39:48 -0600

    finish exercise 2 extra credit exercise.

--- advanced-react-patterns/src/exercise/02.js
+++ advanced-react-patterns/src/exercise/02.js
@@ -8,25 +8,18 @@ function Toggle(props) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
   // 📜 https://reactjs.org/docs/react-api.html#reactchildren
   // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  // return 
-  return React.Children.map(props.children, child => React.cloneElement(child, {on, toggle}))
+  return React.Children.map(props.children, child => {
+    if(typeof child.type === 'string') {
+      return child
+    }
+    return React.cloneElement(child, {on, toggle})
+  })
 }
 
-// 🐨 Flesh out each of these components
-
-// Accepts `on` and `children` props and returns `children` if `on` is true
 const ToggleOn = ({on, children}) => on ? children : null
-
-// Accepts `on` and `children` props and returns `children` if `on` is false
 const ToggleOff = ({on, children}) => !on ? children : null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
 const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
 
 function App() {
@@ -35,6 +28,7 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
         <ToggleButton />
       </Toggle>
     </div>
