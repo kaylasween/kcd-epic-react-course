840261c181d36a545fd95ead7507820ceef31a21
Kayla Sween <kayla@Kaylas-Flightcontrol-Macbook-Pro.local>
2023-02-10 15:09:23 -0600

    finish exercise 3.

--- testing-react-apps/src/__tests__/exercise/02.js
+++ testing-react-apps/src/__tests__/exercise/02.js
@@ -12,7 +12,7 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const [decrement, increment] = container.querySelectorAll('button')
   const message = container.firstChild.querySelector('div')
 
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
 
   fireEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
