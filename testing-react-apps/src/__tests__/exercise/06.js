c5f81732fb13d6541fde414c01fda178bf5f3a68
Kayla Sween <kayla@Kaylas-Flightcontrol-Macbook-Pro.local>
2023-02-13 11:40:43 -0600

    finish extra credit 2.

--- testing-react-apps/src/__tests__/exercise/06.js
+++ testing-react-apps/src/__tests__/exercise/06.js
@@ -3,10 +3,22 @@
 
 import * as React from 'react'
 import {render, screen, act} from '@testing-library/react'
-import { useCurrentPosition } from 'react-use-geolocation'
 import Location from '../../examples/location'
 
-jest.mock('react-use-geolocation')
+beforeAll(() => {
+  window.navigator.geolocation = {
+    getCurrentPosition: jest.fn()
+  }
+})
+
+function deferred() {
+  let resolve, reject
+  const promise = new Promise((res, rej) => {
+    resolve = res
+    reject = rej
+  })
+  return {promise, resolve, reject}
+}
 
 test('displays the users current location', async () => {
   const fakePosition = { 
@@ -16,25 +28,57 @@ test('displays the users current location', async () => {
     }
   }
 
-  let setReturnValue
-  function useMockCurrentPosition() {
-    const state = React.useState([])
-    setReturnValue = state[1] 
-    return state[0]
-  }
+  const {promise, resolve} = deferred()
 
-  useCurrentPosition.mockImplementation(useMockCurrentPosition)
+  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+  window.navigator.geolocation.getCurrentPosition.mockImplementation(
+    callback => {
+      promise.then(() => callback(fakePosition))
+    }
+  )
 
   render(<Location />)
   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
 
-  act(() => {
-    setReturnValue([fakePosition])
+  // If you'd like, learn about what this means and see if you can figure out
+  // how to make the warning go away (tip, you'll need to use async act)
+  // 📜 https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
+  await act(async () => {
+    resolve()
+    await promise
   })
 
   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
   expect(screen.getByText(/latitude/i)).toHaveTextContent(`Latitude: ${fakePosition.coords.latitude}`)
   expect(screen.getByText(/longitude/i)).toHaveTextContent(`Longitude: ${fakePosition.coords.longitude}`)
+})
+
+test('displays error state when there is a problem', async () => {
+  const fakeError = new Error(
+    'Geolocation is not supported or permission denied',
+  )
+
+  const {promise, reject} = deferred()
+
+  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+  window.navigator.geolocation.getCurrentPosition.mockImplementation(
+    (successCallback, errorCallback) => {
+      promise.catch(() => errorCallback(fakeError))
+    }
+  )
+
+  render(<Location />)
+  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+
+  // If you'd like, learn about what this means and see if you can figure out
+  // how to make the warning go away (tip, you'll need to use async act)
+  // 📜 https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
+  await act(async () => {
+    reject()
+  })
+
+  expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+  expect(screen.getByRole('alert')).toHaveTextContent(fakeError.message)
 })
 
 /*
